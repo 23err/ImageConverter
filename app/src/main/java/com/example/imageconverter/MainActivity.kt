ef3d2dacb1302579a2dc9338830b091acbcdfadc
phil <flip.04@mail.ru>
2021-10-26 17:33:33 +0300

    init

---
+++ app/src/main/java/com/example/imageconverter/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.imageconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.imageconverter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var vb: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
